82e5190530643282a9d6c822de50757962e718b3
mannatgupta146 <mannatgupta146@gmail.com>
2025-03-18 23:09:50 +0530

    added

---
+++ src/components/Heart.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import "./styles/style.css";
+
+const Heart = () => {
+  return (
+    <div>
+      <div className="heart">
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
+      <div className="beloved"></div>
+    </div>
+  );
+};
+
+export default Heart;
